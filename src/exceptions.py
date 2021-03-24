1fc90d1c43e386d30233c051529fe0775eba93da
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2021-03-24 23:13:21 +0300

    fix unused exception
    deleted InvalidBoldItalicException because it was never called

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,8 +1,3 @@
 class InvalidHeadingException(Exception):
 	def __init__(self):
 		super().__init__("Invalid heading: more than 6 hashtags (#)")
-
-
-class InvalidBoldItalicException(Exception):
-	def __init__(self):
-		super().__init__("Invalid heading: more than 3 asterisks (*)")
